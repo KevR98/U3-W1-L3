2ba1b983ae850e48f35dd5824d103067040b1a2b
KevR98 <ramil.kevin98@gmail.com>
2025-07-23 15:58:48 +0200

    change the function SingleBook.jsx into class

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,27 +1,30 @@
+import { Component } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
-const SingleBook = (props) => {
-  return (
-    <Container className='my-2'>
-      <Row>
-        <Col xs={12} md={6} lg={4} xl={3} className='g-2'>
-          <Card className='card'>
-            <Card.Img variant='top' src={props.copertina} />
-            <Card.Body className='card-body'>
-              <div className='content'>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>Category: {props.category}</Card.Text>
-                <Card.Text>Price: {props.price}</Card.Text>
-              </div>
-              <div>
-                <Button variant='primary'>ACQUISTA</Button>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+class SingleBook extends Component {
+  render() {
+    return (
+      <Container className='my-2'>
+        <Row>
+          <Col xs={12} md={6} lg={4} xl={3} className='g-2'>
+            <Card className='card'>
+              <Card.Img variant='top' src={this.props.copertina} />
+              <Card.Body className='card-body'>
+                <div className='content'>
+                  <Card.Title>{this.props.title}</Card.Title>
+                  <Card.Text>Category: {this.props.category}</Card.Text>
+                  <Card.Text>Price: {this.props.price}</Card.Text>
+                </div>
+                <div>
+                  <Button variant='primary'>ACQUISTA</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default SingleBook;
